8b8789b3c088be649e19e331ab2209798e45c8e0
JacobLei <17763636521@163.com>
2018-07-01 18:46:49 +0800

    添加unordered_multiset测试代码

---
+++ 08_test_unordered_multiset.cpp
@@ -0,0 +1,48 @@
+#include <unordered_set>
+#include <string>
+#include <ctime>
+#include <stdexcept>
+#include <cstdlib>
+#include <cstdio>
+#include "assist.h"
+
+using namespace std;
+
+void test_unordered_multiset(long& value)
+{
+    unordered_multiset<string> c;
+    char buf[10];
+    clock_t timeStart = clock();
+
+    for(long i=0; i<value; ++i)
+    {
+        try{
+            snprintf(buf, 10, "%d", rand() % 10000);
+            c.insert(string(buf));
+        }
+        catch(exception& p){
+            cout << "i=" << i << " " << p.what() <<endl;
+            abort();
+        }
+    }
+
+    cout << "milli-seconds:" << clock() - timeStart << endl;
+    cout << "unordered_multiset.size() = " << c.size() << endl;
+    cout << "unordered_multiset.max_size() = " << c.max_size() << endl;
+    cout << "unordered_multiset.bucket_count() = " << c.bucket_count() << endl;
+    cout << "unordered_multiset.load_factor() = " << c.load_factor() << endl;
+    cout << "unordered_multiset.max_load_factor() = " << c.max_load_factor() << endl;
+    cout << "unordered_multiset.max_bucket_count() = " << c.max_bucket_count() << endl;
+
+    for(unsigned i=0; i<100; i++)
+    {
+        cout << "bucket #" << i << " has " << c.bucket_size(i) << endl;
+    }
+}
+
+int main()
+{
+    long value = 1000000;
+    test_unordered_multiset(value);
+    return 0;
+}
